a22eb107e43c725e15fb8bede9396fdfee3050fa
vipesh.ap <vipeshrevathi@gmail.com>
2021-07-13 13:09:10 +0530

    added house

--- src/main/java/com/covid/panchayathapp/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.covid.panchayathapp.service;
-
-import com.covid.panchayathapp.model.MemberModel;
-
-public interface MemberService {
-	
-	public MemberModel getMember (Long memberId);
-
-}
